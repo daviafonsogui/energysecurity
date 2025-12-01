bb1dc8080aef80365c420e776abd311456e376b1
daviafonsogui <dagalphonse@gmail.com>
2025-11-30 21:15:00 -0300

    S3

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
+app.use(express.json())
+app.use(cors())
 
 // HTTPS - caminho para certificados
 
